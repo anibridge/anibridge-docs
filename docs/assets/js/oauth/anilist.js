06cecc742ef0c446e9f17633d00de274f5bb1a46
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-02-25 20:32:19 +0000

    fix: AniList no longer requires a redirect_uri in the OAuth query params

--- docs/assets/js/oauth/anilist.js
+++ docs/assets/js/oauth/anilist.js
@@ -1,7 +1,6 @@
 (() => {
   const AUTH_URL = "https://anilist.co/api/v2/oauth/authorize";
   const DEFAULT_CLIENT_ID = "34003";
-  const DEFAULT_REDIRECT_URI = `${window.location.origin}?callback=anilist`;
 
   const registry = window.AniBridgeOAuth;
   if (!registry || typeof registry.registerProvider !== "function") return;
@@ -10,13 +9,11 @@
 
   function makeAuthUrl(opts = {}) {
     const clientId = opts.clientId ?? DEFAULT_CLIENT_ID;
-    const redirectUri = opts.redirectUri ?? DEFAULT_REDIRECT_URI;
     const url = utils.buildUrl(AUTH_URL, {
       client_id: clientId,
-      redirect_uri: redirectUri,
       response_type: "token",
     });
-    return { url, redirectUri };
+    return { url };
   }
 
   registry.registerProvider("anilist", {
@@ -24,11 +21,6 @@
     logoUrl: "https://cdn.simpleicons.org/anilist",
     customArgs: [
       { name: "clientId", label: "Client ID", default: DEFAULT_CLIENT_ID },
-      {
-        name: "redirectUri",
-        label: "Redirect URI",
-        default: DEFAULT_REDIRECT_URI,
-      },
     ],
     tokenLabel: "Access token",
     tokenStorageKey: "anilist_access_token",
